19d51267415fa47899d94899cc915ee1c96e2caa
Nancy967 <beg7520@gmail.com>
2025-01-29 01:08:14 +0800

    home page

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -11,11 +11,10 @@ const Hero = (props: Props) => {
       <div className="container flex flex-col-reverse mx-auto p-8 lg:flex-row">
         <div className="flex flex-col space-y-10 mb-44 m-10 lg:m-10 xl:m-20 lg:mt:16 lg:w-1/2 xl:mb-52">
           <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left">
-            Financial data with no news.
+            觀賞魚大全 -- 探索水族世界的魅力
           </h1>
           <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">
-            Search relevant financial documents without fear mongering and fake
-            news.
+            歡迎來到觀賞魚愛好者的社區！我們不僅有專業的魚種介紹，還提供用戶評論、飼養心得與水族佈景靈感，讓您的水族生活更加精彩。
           </p>
           <div className="mx-auto lg:mx-0">
             <Link
